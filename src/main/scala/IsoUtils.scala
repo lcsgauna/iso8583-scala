0b0fac21af8691e130e1dd0bcfe74127fca66ec2
Lucas <lucas794@hotmail.com>
2021-06-14 20:43:50 -0400

    Adding news functions and comment some functions

--- src/main/scala/IsoUtils.scala
+++ src/main/scala/IsoUtils.scala
@@ -3,10 +3,13 @@ import java.nio.ByteBuffer
 class IsoUtils {
 
   def string2Hex(str: String): String = str.toList.map(_.toInt.toHexString).mkString.toUpperCase
-  def hex2string(hex: String): String = hex.sliding(2, 2).toArray.map(Integer.parseInt(_, 16).toChar).mkString.toUpperCase
-  def hex2Bin(hex: String): String = BigInt(hex, 16).toString(2).mkString.toUpperCase
+  //def hex2string(hex: String): String = hex.sliding(2, 2).toArray.map(Integer.parseInt(_, 16).toChar).mkString.toUpperCase
+  //def hex2Bin(hex: String): String = BigInt(hex, 16).toString(2).mkString.toUpperCase
   def bin2Hex(bin: String): String = BigInt(bin, 2).toString(16).mkString.toUpperCase
   def generateIsoMessage2Bytes(message:String):Array[Byte]= BigInt(message,16).toByteArray
+  def convert2Bytes (messagePack:String):Array[Byte] = BigInt(messagePack,16).toByteArray
+  def formatterNumbers(value:String):String = "0"*(4-value.length)+value
+  def formatterLastNumbers(value:String)= "0"*(2-value.length)+value
 
   def generateBitmap(json: Map[Int, String], dataElement: Int): String =
     dataElement match {
@@ -16,7 +19,24 @@ class IsoUtils {
 
   def selectValues(index: Int, last: Int, json: Map[Int, String]): String = {
     var values: String = ""
-    for (k <- index to last if json.contains(k)) values += json(k)
+    for (value <- index to last if json.contains(value)) values += json(k)
+    values
+  }
+
+  def selectValuesHex(index:Int,last:Int,json:Map[Int,String]):String={
+    var values = ""
+    for(value <- index to last if json.contains(value)) values += json(value)
+    values
+  }
+
+  def selectValuesSizeAndHex(index: Int, last: Int, json: Map[Int, String]): String = {
+    var values: String = ""
+    for (value <- index to last) {
+      if (json.contains(value)) {
+        if (json(value) != json.values.last) values += s"${formatterNumbers(json(value).length.toString)}${string2Hex(json(value))}​​​​​​​"
+        else values += s"${formatterLastNumbers(json(value).length.toString)}​​​​​​​${string2Hex(json(value))}​​​​​​​"
+      }
+    }
     values
   }
 
