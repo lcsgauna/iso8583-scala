355c89e972e1ab97f8f991fcf460343a7f70ef8e
Lucas <lucas794@hotmail.com>
2021-06-14 20:42:13 -0400

    Update Client Socket with more details

--- src/main/scala/ClientSocket.scala
+++ src/main/scala/ClientSocket.scala
@@ -1,31 +1,36 @@
 import java.net._
-import java.io._
+import java.io.{InputStreamReader,IOException}
 
-class ClientSocket {
-
-  def run(header:Array[Byte],message:Array[Byte] ){
+class ClientSocket{
+  def run(header: Array[Byte], isoMessage: Array[Byte]) {
     try {
-      val serverPort: Int = 12345
+
+      val serverPort: Int = 123456
       val host = InetAddress.getByName("localhost")
-      println("Trying to connect to server " + host + " on port: " + serverPort)
-
+      println("\n=============================================================")
+      println(s" Trying connect to a server ${host}​​​​ on port: ${serverPort}​​​​")
       val socket = new Socket(host, serverPort)
-      println("Connected to "+socket.getRemoteSocketAddress)
-      val toServer = new PrintWriter(socket.getOutputStream, true)
-      val fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream))
+      println(s" Connected to ${socket.getInetAddress()}​​")
+      println("=============================================================")
 
-      toServer.print(header)
-      toServer.print(message)
+      val toServer = socket.getOutputStream
+      val fromServer = new BufferedReader (new InputStreamReader(socket.getInputStream()))
+
+      toServer.write(header)
+      toServer.write(isoMessage)
       toServer.flush()
-      println(s"\n Sending message server: \n ${header.mkString}${message.mkString} \n ")
 
-      val line = fromServer.readLine()
-      println("\n Client received: " + line + " from Server")
+      println(s" Sending message server: \n ${header.mkString}​${isoMessage.mkString}​")
+      print("==========================================================")
+
+      val line = fromServer.read()
+      print("\n Client received: " + line + " from Server\n")
 
       toServer.close()
       fromServer.close()
       socket.close()
-    } catch {
+
+    }catch {
       case ex: UnknownHostException => println(ex)
       case e: IOException => println(e)
     }
